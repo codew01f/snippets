d1514c26bff5427abf5666b0efe7bfdb7c2c8f46
codew01f <141761922+codew01f@users.noreply.github.com>
2023-11-25 00:12:25 +0600

    it didnt work

--- codes/playground.cpp
+++ codes/playground.cpp
@@ -17,45 +17,20 @@ using grid = vector<vector<long long>>;
 #define fjr(a) for(int j=0; j<a; ++j)
 
 
-void ftud(grid &ft, ll i, ll jj, ll val){
-  ll r=ft.size();
-  ll c=ft[0].size();
-  
-  ll j;
-  for(; i<r; i += -i&i){
-    for(j=jj; j<c; j += -j&j){
-      ft[i][j]+=val;
-    }
-  }
+void ftud(grid &ft, ll rr, ll cc, ll val){
   return;
 }
 
 grid mkft(grid &g){
-  ll r=g.size();
-  ll c=g[0].size();
-  grid res(r+1, vi(c+1, 0));
-
-  fir(r){
-    fjr(c){
-      ftud(res, i+1, j+1, g[j][i]);
-    }
-  }
+ 
   return res;
 }
 
-ll gt(grid &ft, ll x, ll yy){
-  ll res=0, y;
-  for(; x; x -= x&-x){
-    for(y=yy; y; y -= y&-y){
-      res+=ft[x][y];
-    }
-  }
-  return res;
+ll gt(grid &ft, ll rr, ll cc){
+   return res;
 }
 
-ll ftgs(grid &ft, ll x1, ll y1, ll x2, ll y2){
-   x1++; y1++; x2++; y2++;
-   return gt(ft, x2, y2)-gt(ft, x1-1, y2)-gt(ft, x2, y1-1)+gt(ft, x1-1, y1-1);
+ll ftgs(grid &ft, ll r1, ll c1, ll r2, ll c2){
 }
 
 void solve(){
@@ -74,10 +49,10 @@ void solve(){
     if(q==1){
       ll x, y; cin>>x>>y;
       ll temp = ftgs(ft, x, y, x,y);
-      ftud(ft, y-1, x-1, 1-2*temp);
+      ftud(ft, x-1, y-1, 1-2*temp);
     }
     else{
-      ll y1, x1, y2, x2; cin>>x1>>y1>>x2>>y2;
+      ll y1, x1, y2, x2; cin>>y1>>x1>>y2>>x2;
       cout<<ftgs(ft, x1-1, y1-1, x2-1, y2-1)<<"\n";
     }
   }
