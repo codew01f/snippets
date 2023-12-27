ce9033399477729f1162b8ae7b8c3b11c944dd1f
codew01f <141761922+codew01f@users.noreply.github.com>
2023-12-28 00:31:30 +0600

    ...
    Signed-off-by: codew01f <141761922+codew01f@users.noreply.github.com>

--- graphs/spaths/sssp.cpp
+++ graphs/spaths/sssp.cpp
@@ -3,7 +3,7 @@ vi sssd(vector<vector<pii>> &edg, ll s){ //dijkstras' alg
   ll cn=edg.size();
   vi dis(cn, INT_MAX);
   dis[s]=0;
-  vi vst(cn+1, 0);
+  vi vst(cn, 0);
   priority_queue<pii, vector<pii>, greater<pii>> call;
 
   call.push({0, s});
