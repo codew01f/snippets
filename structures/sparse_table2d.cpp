b29cc9f377385211ee80f6aca12e2a2001f53b3c
codew01f <141761922+codew01f@users.noreply.github.com>
2023-12-27 16:29:13 +0600

    int to ll in st2d
    Signed-off-by: codew01f <141761922+codew01f@users.noreply.github.com>

--- structures/sparse_table2d.cpp
+++ structures/sparse_table2d.cpp
@@ -1,16 +1,14 @@
 
 vector<grid> mkst(grid g){
-  int r=g.size();
-  int c=g[0].size();
-  int k=log2(min(r, c)-1);
+  ll r=g.size();
+  ll c=g[0].size();
+  ll k=log2(min(r, c)-1);
 
   vector<grid> res(k+1, grid(r, vi(c, 0)));
-  fir(r){
-    fjr(c){
+  fir(r) fjr(c){
       res[0][i][j]=g[i][j];
-    }
   }
-  for(int p=0; p<k; ++p){
+  for(ll p=0; p<k; ++p){
     fir(r-(2<<p)+1){
       fjr(c-(2<<p)+1){
         ll m1=min(res[p][i][j], res[p][i][j+(1<<p)]);
@@ -23,11 +21,11 @@ vector<grid> mkst(grid g){
   return res;
 }
 
-ll stgm(vector<grid> st, int r, int c, int k){ //0 indexed;
+ll stgm(vector<grid> st, ll r, ll c, ll k){ //0 indexed;
   if(k==1) return st[0][r][c]; 
-  int p = log2(k-1);
-  int rr = r+k-1;
-  int cc = c+k-1;
+  ll p = log2(k-1);
+  ll rr = r+k-1;
+  ll cc = c+k-1;
   ll mi1=min(st[p][r][c], st[p][rr-(1<<p)+1][c]);
   ll mi2=min(st[p][r][cc-(1<<p)+1], st[p][rr-(1<<p)+1][cc-(1<<p)+1]);
   return min(mi1, mi2);
