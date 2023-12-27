ada0a153f14d7e5faf3979ea6979dfbf14c3c657
codew01f <141761922+codew01f@users.noreply.github.com>
2023-12-28 00:41:38 +0600

    ...
    Signed-off-by: codew01f <141761922+codew01f@users.noreply.github.com>

--- graphs/spaths/sssp_dag.cpp
+++ graphs/spaths/sssp_dag.cpp
@@ -1,11 +1,12 @@
 
-vi sssd(ll cn, ll ce, ll s, vector<vector<pii>> &edg, vi &ts){
-  vi dis(cn+1, INT_MAX);
+vi sssd(vector<vector<pii>> &edg, vi &ts, ll s){
+  ll cn=edg.size();
+  vi dis(cn, INT_MAX);
   dis[s]=0;
 
   ll idx=0;
   while(ts[idx]!=s) idx++;
-  while(idx<cn){
+  while(idx<cn-1){
     ll at=ts[idx];
     for(auto [to, wt]:edg[at]){
       dis[to]=min(dis[to], dis[at]+wt);
