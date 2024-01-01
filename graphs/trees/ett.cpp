bb51e3a224164cd6f66537f8e660cf09d01b66e7
codew01f <141761922+codew01f@users.noreply.github.com>
2024-01-01 18:48:12 +0600

    eula tour

--- graphs/trees/ett.cpp
+++ graphs/trees/ett.cpp
@@ -1,18 +1,19 @@
 
-void et(grid &edg, ll at, ll pt grid &tr, ll &id){
-  tr[0][idx]=sn; //=val[sn]
-  tr[1][sn]=id++;
+void et(grid &edg, ll at, ll pt, grid &tr, ll &id){
+  tr[0][id]=at; //=val[at]
+  tr[1][at]=id++;
 
   for(ll to: edg[at]) if(to-pt){
     et(edg, to, at, tr, id);
   }
-  tr[0][idx]=at;
-  tr[2][at]=id++
+  tr[0][id]=at;
+  tr[2][at]=id++;
+  return;
 }
 
 grid etour(grid &edg, ll rt){
   ll cn=edg.size(), id=1;
-  vi tour(3, vi(2*cn, 0));
+  vi tour={vi(2*cn, 0), vi(cn), vi(cn)};
   et(edg, rt, 0, tour, id);
   return tour;
 }
