509e775d94acbe80147ff58b522ac40e8ccb02ae
codew01f <141761922+codew01f@users.noreply.github.com>
2024-02-04 16:11:19 +0600

    okay
    Signed-off-by: codew01f <141761922+codew01f@users.noreply.github.com>

--- graphs/spaths/apsp.cpp
+++ graphs/spaths/apsp.cpp
@@ -1,17 +1,14 @@
 
-grid apsp(vector<vector<pii>> &edg){ //floyd-warshall alg
-  ll cn=edg.size();
-  grid dis(cn, vi(cn, INT_MAX));
-
+grid apsd(vector<vector<pii>> &edg){ //floyd-warshall alg
+  ll cn=edg.size(); ll INF = 0x3f3f3f3f3f3f3f3f;
+  grid asd(cn, vi(cn, INF));
+  
   fir(cn) for(auto [to, wt]: edg[i]){
-    dis[i][to]=wt;
+    asd[i][to]=min(asd[i][to], wt);
   }
+  fir(cn) asd[i][i]=0;
   for(int k=0; k<cn; ++k){
-    fjr(cn) fir(cn){
-      if(dis[j][k]+dis[k][i]<dis[j][i]){
-        dis[j][i]=dis[j][k]+dis[k][i];
-      }    
-    }
+    fjr(cn) fir(cn) asd[j][i]=min(asd[j][i], asd[j][k]+asd[k][i]);
   }
-  return dis;
+  return asd;
 }
